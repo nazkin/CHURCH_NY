99406a1d5bf8f8a48444fa3d9048294e2979f030
Naz Kin <nazkin@pop-os.localdomain>
2023-11-12 02:46:04 -0500

    updating service for emailing, emails can officially be sent using the site

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,32 +1,63 @@
 import * as React from "react"
 import { Link } from "gatsby"
+
 import {
   Grid,
-  Container,
+  Alert,
   TextField,
   Button
 } from '@mui/material'
+import emailjs from '@emailjs/browser';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import contactImg from '../../public/c3.jpg'
 
-const UsingDSG = () => {
+const SERVICE = 'service_l2h0844'
+const TEMPLATE = 'template_smhk7ll'
+const PUBLIC_KEY = 'Bnn2RnFZ7su91pwrn'
 
+const UsingDSG = () => {
+  const form = React.useRef()
   const [name, setName] = React.useState("")
   const [email, setEmail] = React.useState("")
   const [phone, setPhone] = React.useState("")
   const [message, setMessage] = React.useState("")
+  const [success, setSuccess] = React.useState(false)
+  const [error, setError] = React.useState(false)
+
+  React.useEffect(() => {
+
+    setTimeout(() => {
+      setSuccess(false)
+      setError(false)
+    }, 3000)
+
+  }, [message]);
 
   const onSubmit = (e) => {
     e.preventDefault()
-    const res = {
+    const params = {
       name,
       email,
-      phone,
-      message
+      "user_phone": phone,
+      "user_msg": message
     }
-    console.log(`Form content ${name}`)
+
+    emailjs.send(SERVICE, TEMPLATE, params, PUBLIC_KEY)
+      .then((result) => {
+        console.log(result);
+
+        setName("")
+        setEmail("")
+        setPhone("")
+        setMessage("")
+        setSuccess(true)
+
+      }, (error) => {
+        setError(true)
+        console.log(error.text);
+      });
   }
 
   return <Layout>
@@ -43,10 +74,11 @@ const UsingDSG = () => {
         alignItem: "center"
       }}>
       <Grid md={6} style={{ padding: 20 }}>
+
         <TextField
           id="name-input"
           label="Name"
-          type="Name"
+          type="name"
           autoComplete="Your Name"
           variant="outlined"
           style={{
@@ -61,7 +93,7 @@ const UsingDSG = () => {
         <TextField
           id="email-input"
           label="Email"
-          type="Email"
+          type="email"
           autoComplete="Your Email"
           variant="outlined"
           style={{
@@ -76,7 +108,7 @@ const UsingDSG = () => {
         <TextField
           id="phone-input"
           label="Phone"
-          type="Phone"
+          type="phone"
           autoComplete="Your Phone"
           variant="outlined"
           style={{
@@ -91,7 +123,7 @@ const UsingDSG = () => {
         <TextField
           id="message-input"
           label="Message"
-          type="Message"
+          type="message"
           autoComplete="Your Message"
           variant="outlined"
           multiline={true}
@@ -105,15 +137,27 @@ const UsingDSG = () => {
             setMessage(e.target.value)
           }}
         />
-        <Button onClick={(e) => onSubmit(e)} variant="outlined" style={{ width: 100, marginTop: 50 }}>Submit</Button>
+        <div style={{ height: 75, marginTop: 15 }}>
+          {success && <Alert variant="filled" severity="success">
+            Contact message was sent successfully. Thank you!
+          </Alert>
+          }
+          {error && <Alert variant="filled" severity="error">
+            Unable to send the message. Please try again !
+          </Alert>
+          }
+        </div>
+
+        <Button onClick={(e) => onSubmit(e)} type="submit" variant="outlined" style={{ width: 100 }}>Submit</Button>
+
       </Grid>
       <Grid md={5} style={{
-        background: "whitesmoke",
+        background: "lightsteelblue",
+        border: "30px solid lightsteelblue",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItem: "center",
-        padding: 20
       }}>
         <img src={contactImg} />
       </Grid>
@@ -121,6 +165,6 @@ const UsingDSG = () => {
   </Layout>
 }
 
-export const Head = () => <Seo title="Using DSG" />
+export const Head = () => <Seo title="Contact" />
 
 export default UsingDSG
