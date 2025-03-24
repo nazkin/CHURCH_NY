6701bdff82a82991eaed262963015bd5e8a4d3ff
Naz Kin <nazkin@pop-os.localdomain>
2025-03-24 00:11:36 -0400

    help build

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,37 +14,52 @@ const AboutContent = ({ language }) => {
     <Box width="80%" height="100%" marginTop={5} justifySelf={"center"}>
       <Grid container paddingTop={10}>
         <Grid item md={6} style={{ padding: 5 }}>
-          <Box display={"flex"} width="80%" height="80%" justifyContent={"center"} alignItems={
-            "center"
-          }><StaticImage src="../images/about.jpg" placeholder="blurred" />
+          <Box
+            display={"flex"}
+            width="80%"
+            height="80%"
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <StaticImage src="about.jpg" placeholder="blurred" />
           </Box>
         </Grid>
-        <Grid item md={6} style={{ padding: 5, display: "flex", flexDirection: "column", justifyContent: "left"}}>
+        <Grid
+          item
+          md={6}
+          style={{
+            padding: 5,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "left",
+          }}
+        >
           <Stack width="100%" height="100%" direction="column" gap={2}>
             <Box>{ABOUT_CONTENT[language].welcome}</Box>
             <Box>{ABOUT_CONTENT[language].about}</Box>
           </Stack>
         </Grid>
       </Grid>
-      <Box sx={{ width: '100%'}}>
-        <Divider sx={{ borderBottomWidth: 2, borderColor: 'grey.500' }} />
+      <Box sx={{ width: "100%" }}>
+        <Divider sx={{ borderBottomWidth: 2, borderColor: "grey.500" }} />
       </Box>
       <Grid container>
         <Grid item xs={12} md={12} style={{ padding: 5 }}>
-          <Box><h2>{ABOUT_CONTENT[language].histroyTitle}</h2></Box>
+          <Box>
+            <h2>{ABOUT_CONTENT[language].histroyTitle}</h2>
+          </Box>
           {ABOUT_CONTENT[language].history}
         </Grid>
       </Grid>
       <Grid container>
-      <Grid item xs={12} md={12} style={{ padding: 5 }}>
-        </Grid>
+        <Grid item xs={12} md={12} style={{ padding: 5 }}></Grid>
         <Grid item xs={12} md={12} style={{ padding: 5 }}>
           <h2>{ABOUT_CONTENT[language].faq}</h2>
           <FAQ language={language} />
         </Grid>
       </Grid>
     </Box>
-  )
+  );
 }
 
 export const Head = () => <Seo title="About Page" />
