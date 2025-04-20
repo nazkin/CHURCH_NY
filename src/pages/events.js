7a5ae26b5b4756170d1cefdbed97b83a5255fced
Naz Kin <nazkin@pop-os.localdomain>
2025-04-20 00:48:24 -0400

    update script

--- src/pages/events.js
+++ src/pages/events.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import { Footer } from "../components/Footer"
 import { EVENTS_CONTENT } from "../constants/content/events"
 import { Grid, Box, Stack, Divider, Typography } from "@mui/material";
-import { Script } from 'gatsby';
+import { Script } from "react-dom";
 
 
 const EventsContent = ({ language }) => {
@@ -19,18 +19,44 @@ const EventsContent = ({ language }) => {
         console.log("RUN USE EFFECT!!!")
     }, []);
 
-    <Box sx={{ width: '80%', maxWidth: '1000px', backgroundColor: '#ffffff', padding: 2, borderRadius: '8px', boxShadow: 2, paddingTop: '100px', paddingBottom: 130, margin: '0 auto'}} justifySelf={'center'}>
-        <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 3 }}>
-            {EVENTS_CONTENT[language].title}
-        </Typography>
-        <Box width="100%" height={"100%"} minWidth={"500px"} minHeight={"500px"} display="flex" justifyContent={"center"} alignItems={"center"}>
-            <div style={{display: "flex", width: "80%", height: "80%"}} data-events-calendar-app data-project-id="proj_rcajbWhaMnIMxDi4zif5R" ></div>
-            <Script 
-                // strategy="post-hydrate" 
-                // onLoad={() => setScriptLoaded(true)} 
-                type="text/javascript" src="//dist.eventscalendar.co/embed.js" />
-        </Box>
-  </Box>
+    <Box
+      sx={{
+        width: "80%",
+        maxWidth: "1000px",
+        backgroundColor: "#ffffff",
+        padding: 2,
+        borderRadius: "8px",
+        boxShadow: 2,
+        paddingTop: "100px",
+        paddingBottom: 130,
+        margin: "0 auto",
+      }}
+      justifySelf={"center"}
+    >
+      <Typography variant="h4" sx={{ textAlign: "center", marginBottom: 3 }}>
+        {EVENTS_CONTENT[language].title}
+      </Typography>
+      <Box
+        width="100%"
+        height={"100%"}
+        minWidth={"500px"}
+        minHeight={"500px"}
+        display="flex"
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <div
+          style={{ display: "flex", width: "80%", height: "80%" }}
+          data-events-calendar-app
+          data-project-id="proj_rcajbWhaMnIMxDi4zif5R"
+        ></div>
+        <Script
+          // strategy="post-hydrate"
+          // onLoad={() => setScriptLoaded(true)}
+          src="//dist.eventscalendar.co/embed.js"
+        />
+      </Box>
+    </Box>;
 }
 
 const Events = () => {
