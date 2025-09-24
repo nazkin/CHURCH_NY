9cd8290f50d6c673a9e6c0cd7bcfe5cb4438ef6d
Taras <taras.kinash.9@gmail.com>
2025-09-24 19:44:25 +0200

    Bulleting September 21 2025

--- src/pages/bulletin.js
+++ src/pages/bulletin.js
@@ -217,7 +217,7 @@ const BulletinContent = ({ language }) => {
       }}
     >
       <Grid xs={12} md={6} px={"10px"}>
-        <PdfDisplay pdfUrl={"/september142025.pdf"} />
+        <PdfDisplay pdfUrl={"/september09212025.pdf"} />
       </Grid>
       <GlobalStyles
         styles={{
