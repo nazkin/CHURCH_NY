416053d009922e6e54c6ea1733f3cdc637c5a427
Taras <taras.kinash.9@gmail.com>
2025-10-26 17:53:23 -0400

    Bulletin for October 26 2025

--- src/pages/bulletin.js
+++ src/pages/bulletin.js
@@ -217,7 +217,7 @@ const BulletinContent = ({ language }) => {
       }}
     >
       <Grid xs={12} md={6} px={"10px"}>
-        <PdfDisplay pdfUrl={"/october192025.pdf"} />
+        <PdfDisplay pdfUrl={"/october262025.pdf"} />
       </Grid>
       <GlobalStyles
         styles={{
